c59f80c069e3442a4ee720d54824ff0b2cf5b166
MKotliarenko <kotliarenkom282@gmail.com>
2022-01-14 10:47:34 +0100

    12

---
+++ src/components/Header.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export function Header() {
+    return (<header className='header'>
+        <img src='https://upload.wikimedia.org/wikipedia/commons/a/ab/Logo_TV_2015.png'/>
+    </header>);
+}
